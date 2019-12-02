a2760e6a3289b17837e4473ef8644a929d805179
Christian Tomyama <christiantomyama@gmail.com>
2019-12-02 07:17:26 -0300

    implementando API

---
+++ RockPaperScissor.Services.Api/Controllers/RpsTournamentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RockPaperScissor.Domain.Interfaces;
+using RockPaperScissor.Domain.Tournament;
+
+namespace RockPaperScissor.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RpsTournamentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get([FromBody] JArray array)
+        {
+            try
+            {
+                Tournament tournament = RpsTournament.Build();
+                IPlayer player = tournament.FindWinner(array);
+
+                return $"[\"{player.Name}\", \"{player.Command}\"]";
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
---
+++ RockPaperScissor.Services.Api/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RockPaperScissor.Domain.Tournament;
+
+namespace RockPaperScissor.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return new RockPaperScissor.Domain.Tournament.Player("ASDS","A").ToString();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] List<object> value)
+        {
+            Console.WriteLine(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
